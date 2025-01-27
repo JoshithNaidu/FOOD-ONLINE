db070baeb294bcf7d84f02871d2313585ee9076c
Joshith Naidu <tadijoshithnaidu@gmail.com>
2025-01-27 19:21:23 +0530

    Basic Frontend template setup, default sqlite3 database connection and superuser created

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'foodOnline_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'], # Add this line to the TEMPLATES list    
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
